98ca8d82f5521beccf5c9161490954024777153a
David Sun <iamslash@gmail.com>
2016-04-04 17:15:08 +0900

    first finished. well works.

--- monohttpd/Program.cs
+++ monohttpd/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading;
 
 namespace monohttpd
 {
 	class MainClass
 	{
+		// usage:
+		// 	mono monohttpd 127.0.0.1 8080
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			if (args == null || args.Length != 2)
+			{
+				Console.WriteLine("usage) mono monohttpd.exe <ip> <port>");
+				Console.WriteLine("example) mono monohttpd.exe 127.0.0.1 8080");
+			}
+
+			string ip 	= args [0];
+			int port 	= int.Parse(args[1]);	
+
+			int MaxThreadsCount = Environment.ProcessorCount * 2;
+			ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+			ThreadPool.SetMinThreads(2, 2);
+			new HttpServer(ip, port);
 		}
 	}
 }
